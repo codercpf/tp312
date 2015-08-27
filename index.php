e536dc275ce018e3f97d87ea8babb1d942041eeb
Mediterranean <makingdifference@aliyun.com>
2015-08-28 00:27:18 +0800

    leart tp first edition

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	// 确定应用名称
+	define('APP_NAME','Home');
+	// 确定应用路劲
+	define('APP_PATH','./Home/');
+	//开启调试模式
+	define('APP_DEBUG','True');
+	// 引入核心文件
+	require './ThinkPHP/ThinkPHP.php';
+?>
