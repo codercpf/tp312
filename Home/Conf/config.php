4a6ebf86fc673f180513a2b7eb5175c7e73969cf
Mediterranean <makingdifference@aliyun.com>
2015-08-28 14:01:36 +0800

    practice in company 828

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -1,6 +1,24 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-    'URL_PATHINFO_DEPR' => '/'
+    'URL_PATHINFO_DEPR' => '/',          //修改URL的分隔符
+
+    'TMPL_L_DELIM'  =>  '<{',           //修改左定界符
+    'TMPL_R_DELIM'  =>  '}>',            //修改右定界符
+
+    /*
+    'DB_TYPE'       =>  'mysql',
+    'DB_HOST'       =>  'localhost',
+    'DB_NAME'       =>  'thinkphp',
+    'DB_USER'       =>  'root',
+    'DB_PWD'        =>  'root',
+    'DB_PORT'       =>  '3306',
+    */
+
+    'DB_PREFIX'     =>  'tp_',
+
+    //使用DSN方式配置数据库信息，同时存在的话，DSN优先级高
+    'DB_DSN'        =>  'mysql://root:root@localhost:3306/thinkphp',
+
+    'SHOW_PAGE_TRACE'   => 'true'
 );
-?>
--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -6,11 +6,52 @@ class IndexAction extends Action
     public function index()
     {
 //        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8');
-        echo "Hello World!";
+
+//        echo "Hello World!";
+
+//        $m=new Model('User');           //通过New Model操作数据库
+
+        $m=M('User');       //M()方法等效于 new Model()方法
+
+        // // // // // //选择数据
+//        $arr=$m->select();
+
+//        $arr=$m->find(1);   //find($id)方法获取单条数据
+
+//        $arr=$m->where('id=1')->getField('username');
+
+//        var_dump($arr);
+
+
+        // // // // // //增加数据           ---------- 返回值新增的Id号
+//        $m->username='李四';
+//        $m->sex=1;
+//        $m->add();
+//          $countadd=$m->add();
+
+        // // // // // //删除数据           ---------- 返回值为受影响行数
+//  //         $m->delete(3);
+//        $m->where('id=2')->delete();
+
+        // // // // // //更新数据           ------------ 返回值是受影响的行数
+         $data['id']=1;
+         $data['username']='Lili';
+         $count = $m->save($data);                   //save()函数要传入一个修改过的参数。
+         echo $count;
+
+//        $name='常鹏飞';
+
+//        $this->assign('data',$arr[0]['username']);
+
+//        $this->assign('data',$arr);
+
+        $this->display();
     }
 
     public function showself()
     {
-        echo "欢迎你".$_GET['name']." 你的年龄".$_GET['age'];
+//        echo "欢迎你".$_GET['name']." 你的年龄".$_GET['age'];
+
+        $this->display();
     }
 }
